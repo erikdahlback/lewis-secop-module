8f87e54b2304495cc7bbe6f1aec92194a883c9c6
Michael Wedel <michael.wedel@esss.se>
2017-03-14 14:41:35 +0100

    + First attempt to write cache + Addning .gitignore

--- secop/__init__.py
+++ secop/__init__.py
@@ -1,5 +1,7 @@
 from lewis.devices import Device
 from lewis.adapters.epics import EpicsAdapter, PV
+import threading
+from time import sleep
 
 from secop.loggers import initLogging
 
@@ -10,15 +12,34 @@ from secop.client.baseclient import Client
 framework_version = '1.0.2'
 
 
-class SecopDevice(Device):
-    dummy = 10.0
+def cmd_loop(lock, write_cache, fn):
+    while True:
+        with lock:
+            while write_cache:
+                cmd = write_cache.pop(0)
+                fn(*cmd)
+        sleep(0.234)
 
+
+class SecopDevice(Device):
     def __init__(self, host, port):
         super(SecopDevice, self).__init__()
 
         self._sc = Client({'connectto': host, 'port': port}, autoconnect=False)
         self._sc.startup(async=True)
         self.log.info('Modules: %s', self._sc.modules)
+        self._write_cache = []
+
+        self._lock = threading.RLock()
+
+        self._thread = threading.Thread(target=cmd_loop, args=(
+            self._lock, self._write_cache, self._sc.setParameter))
+        self._thread.daemon = True
+        self._thread.start()
+
+    def set_parameter(self, module, param, value):
+        with self._lock:
+            self._write_cache.append((module, param, value))
 
 
 class SecopEpicsInterface(EpicsAdapter):
@@ -29,13 +50,13 @@ class SecopEpicsInterface(EpicsAdapter):
             for param in self.device._sc.getParameters(module):
                 def create_getter(mod, par):
                     def getter(obj):
-                        return self.device._sc.queryCache(mod, par).value
+                        return obj.device._sc.queryCache(mod, par).value
 
                     return getter
 
                 def create_setter(mod, par):
                     def setter(obj, value):
-                        return self.device._sc.setParameter(mod, par, value)
+                        return obj.device.set_parameter(mod, par, value)
 
                     return setter
 
@@ -43,7 +64,8 @@ class SecopEpicsInterface(EpicsAdapter):
                 #
                 self.log.info('Installing property on interface: %s', attr_name)
                 #
-                setattr(type(self), attr_name, property(create_getter(module, param), create_setter(module, param)))
+                setattr(type(self), attr_name,
+                        property(create_getter(module, param), create_setter(module, param)))
                 #
                 self.pvs[module + ':' + param] = PV(attr_name)
 
